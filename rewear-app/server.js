97a603bb382469328c988167c13a861b2624dcda
Patelharsh6 <harshpatel010606@gmail.com>
2025-07-27 12:59:38 +0530

    server.js basic code and route adding

--- rewear-app/server.js
+++ rewear-app/server.js
@@ -97,7 +97,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-// --- Start The Server ---
+// --- Start The Server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
